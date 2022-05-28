002f48c80abffa3ddfc753e18bec5113d966f595
Guilherme Pantoja <guilhermematheus166@gmail.com>
2022-05-28 15:47:09 -0300

    Add files via upload

---
+++ Atividade4/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("=============================================================");
+Console.WriteLine("Atividade 4 - Algoritmo de reajuste de salários!");
+int quantidadeFunc = 0;
+double salario = 0;
+double salarioNovo = 0;
+int qtdFunc = 10;
+
+int i = 0;
+
+for (i = 0; i < qtdFunc; i++)
+{
+    Console.Write("Informe o salário do funcionário: ");
+    salario = Convert.ToDouble(Console.ReadLine());
+    if(salario <= 300)
+    {
+        salarioNovo = salario * 1.50;
+    }
+    else
+    {
+        salarioNovo = salario + ((salario * 30) / 100);
+    }
+    Console.WriteLine("Salário novo: " + salarioNovo);
+    Console.WriteLine("========================================");
+}
---
+++ Atividade5/Program.cs
@@ -0,0 +1,51 @@
+﻿Console.WriteLine("=============================================================");
+Console.WriteLine("Atividade 5 - Algoritmo leitura de matrículas.");
+
+string maiorMatricula;
+double maiorAltura;
+
+string menorMatricula;
+double menorAltura;
+
+string matricula;
+double altura;
+
+Console.Write("Informe a matrícula do Aluno: ");
+matricula = Console.ReadLine();
+Console.Write("Informe a altura do Aluno: ");
+altura = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("");
+
+maiorMatricula = matricula;
+maiorAltura = altura;
+menorMatricula = matricula;
+menorAltura = altura;
+
+int i = 1;
+while(i < 10)
+{
+    Console.Write("Informe a matricula do Aluno: ");
+    matricula = Console.ReadLine();
+    Console.Write("Informe a altura do Aluno: ");
+    altura = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("");
+
+    if(maiorAltura < altura)
+    {
+        maiorAltura = altura;
+        maiorMatricula = matricula;
+    }
+
+    if(menorAltura > altura)
+    {
+        menorAltura = altura;
+        menorMatricula = matricula;
+    }
+    i++;
+}
+Console.WriteLine("");
+Console.WriteLine("Maior Aluno: ");
+Console.WriteLine("Matrícula: {0}\nAltura: {1}", maiorMatricula, maiorAltura);
+Console.WriteLine("");
+Console.WriteLine("Menor Aluno: ");
+Console.WriteLine("Matrícula: {0}\nAltura: {1}", menorMatricula, menorAltura);
